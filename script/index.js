bf965a25257a2c1fe976905522486f71130a03f5
Montana Hollinger <Tannerho21@gmail.com>
2019-11-13 14:37:23 -0500

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,20 @@
+const START_HP = 20;
+const START_SP = 10;
+const DEFAULT_ATK = 5;
+const DEFAULT_DEF = 5;
+const DEFAULT_TEK = 5;
+
+
+let Player0;
+let Player1;
+
+let gameBox;
+let headerBox;
+let graphicsBox;
+let barsBox;
+let controlsBox;
+let outputBox;
+
 class Fighter {
   constructor(name, charaName) {
     //'contructor' is in all JS classes
@@ -5,11 +22,11 @@ class Fighter {
 
     // Set all of our deafult values for this new fighter here
     this.name = name;
-    this.hp = 20;
-    this.sp = 10;
-    this.atk = 5;
-    this.def = 5;
-    this.tek = 5;
+    this.hp = START_HP;
+    this.sp = START_SP;
+    this.atk = DEFAULT_ATK;
+    this.def = DEFAULT_DEF;
+    this.tek = DEFAULT_TEK;
     this.charaName = charaName;
   }
   attack(target) {
@@ -27,13 +44,19 @@ class Fighter {
   }
 }
 
-
-let Player0;
-let Player1;
-
 function startup() {
   Player0 = new Fighter("Crash", "crashr");
   Player1 = new Fighter("Sam", "saml");
+
+  gameBox = document.getElementById('gameBox');
+  headerBox = document.getElementById('headerBox');
+  graphicsBox = document.getElementById('graphicsBox');
+  barsBox = document.getElementById('barsBox');
+  controlsBox = document.getElementById('controlsBox');
+  outputBox = document.getElementById('outputBox');
+
+  graphicsBox.innerHTML = '<img id ="' + Player0.charaName + '" src="img/' + Player0.charaName + '_idle.png" alt="' + Player0.name + '" class="fighterIMG">'
+  graphicsBox.innerHTML += '<img id ="' + Player1.charaName + '" src="img/' + Player1.charaName + '_idle.png" alt="' + Player1.name + '" class="fighterIMG">'
 
   console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk)
   console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk)
